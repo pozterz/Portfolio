3cf1a04f3b9c2e7023cb297f6bf11833dd3f17fd
pozterz <pozterz2@gmail.com>
2017-07-02 04:44:28 +0700

    add profile and github repos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { GithubService } from './github.service';
+import { GithubComponent } from './github/github.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
+    GithubComponent,
+    ProfileComponent,
+    SkillsComponent,
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    HttpModule,
+    BrowserModule,
   ],
-  providers: [],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/repos.ts
@@ -0,0 +1,7 @@
+export class Repos {
+    id: number;
+    name: string;
+    html_url: string;
+    description: string;
+    fork: boolean;
+}
